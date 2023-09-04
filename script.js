fa4518b8dfe103179f9374ee99999f74a4664a46
LimpanGH <Linusj555@gmail.com>
2023-09-04 16:17:37 +0200

    added styling and img

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
-// Objevtive
+// Objective
 // Funktion för att räkna ut hur långt åska färdas per sekund
 // funktion för att ta input i sekunder från användaren 
 // Return the result and present it with explanatory text
 
-
-// const travelPerSecond = () => {
-//     document.getElementById("input1")
-// let distance = input1 * 0.33;
-// return distance;
-// }
-// console.log(`Sound travels by 0.33 kometers per second and x seconds tells us that thunder struck ${travelPerSecond()} kilometers away`)
+  
+const numberEntered = document.querySelector("#input1");
+const output = document.getElementById("output1");
 
 
-// function travelPerSecond(){
-    
-//     data[0] = document.getElementsByName('input1')[0].value;
-
-//     console.log(data); // Check the console for the values for every click.
-// return data;   
-
-let numberEntered = document.getElementById("input1");
-
-numberEntered.addEventListener("input", function() {
-    let b = this.value;
-    let c = b * 0.33;
+numberEntered.addEventListener("input", (e) => {
+    const b = e.target.value;
+    const c = b * 0.33;
     output.value = c;
 })
 
-let output = document.getElementById("output1");
-
+output.addEventListener("input", (e) => { 
+    numberEntered.value = e.target.value / 0.33;
+})
--- test.js
+++ test.js
@@ -1,60 +1,10 @@
-// The scope of `random` is too loose 
+const purchases = [2,4,6,8,4,8,4,3,7,9,8,4,3,56,9];
 
+let total = 0;
 
-const getRandEvent = () => {
-    const random = Math.floor(Math.random() * 3);
-  if (random === 0) {
-    return 'Marathon';
-  } else if (random === 1) {
-    return 'Triathlon';
-  } else if (random === 2) {
-    return 'Pentathlon';
-  }
-};
+for (let i = 0; i < purchases.length; i++) {
+    total += purchases[i];
+    console.log(purchases[i]);
+}
 
-// The scope of `days` is too tight 
-const getTrainingDays = event => {
-    let days;
-  if (event === 'Marathon') {
-     days = 50;
-  } else if (event === 'Triathlon') {
-     days = 100;
-  } else if (event === 'Pentathlon') {
-     days = 200;
-  }
-
-  return days;
-};
-
-
-const name = 'Nala';
-const name2 = 'Warren';
-
-// The scope of `name` is too tight 
-const logEvent = (name, event) => {
-  console.log(`${name}'s event is: ${event}`);
-};
-
-const logTime = (name, event) => {
-  console.log(`${name}'s time to train is: ${days} days`);
-};
-
-
-
-const event = getRandEvent();
-const days = getTrainingDays(event);
-// Define a `name` variable. Use it as an argument after updating logEvent and logTime 
-
-logEvent(name, event);
-logTime(name, days);
-
-
-
-const event2 = getRandEvent();
-const days2 = getTrainingDays(event2);
-
- 
-logEvent(name2, event2);
-logTime(name2, days2);
-
-
+console.log(`Total is: $${total}`);
